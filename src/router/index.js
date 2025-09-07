b64a7734d3f842bca079b4675d7561f8cf9dccaa
Sherry Wu <shw18731@gmail.com>
2025-09-07 17:46:47 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/todolist',
+      name: 'todolist',
+      meta: { requiresAuth: true, title: 'Todo List' },
+      component: () => import('../views/TodoListView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      meta: { title: '登入' },
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      meta: { title: '註冊' },
+      component: () => import('../views/RegisterView.vue')
+    }
+  ],
+})
+
+// 每次路由改變時修改 title
+router.afterEach((to) => {
+  document.title = to.meta.title || 'Online Todo List'
+})
+
+// 導航守衛
+router.beforeEach((to, from, next) => {
+  const token = document.cookie.replace(
+    /(?:(?:^|.*;\s*)vue3-todolist-token\s*=\s*([^;]*).*$)|^.*$/,
+    '$1',
+  )
+
+  if (to.meta.requiresAuth && !token) {
+    next("/login");  // 未登入導向 login
+  } else if ((to.path === "/login" || to.path === "/register") && token) {
+    next("/todolist") // 已登入直接到至 todoList
+  } else {
+    next(); // 通過驗證
+  }
+});
+
+export default router
